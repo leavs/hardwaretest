86532ccb6b09e37c045ab3a06639b471973e8813
leavs <leavs_o@126.com>
2019-01-10 11:00:35 +0800

    Initial commit for IMX6U CPU boards

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -233,9 +233,9 @@ void MainWindow::BoardSetting()
 
 void MainWindow::boardInit()
 {
-    ui->radioButton_imx6q->setChecked(true);
+    ui->radioButton_imx6q->setDisabled(true);
     ui->radioButton_imx6d->setDisabled(true);
-    ui->radioButton_imx6u->setDisabled(true);
+    ui->radioButton_imx6u->setChecked(true);
     ui->radioButton_am335x->setDisabled(true);
     ui->radioButton_bbb_exp->setDisabled(true);
     BoardSetting();
